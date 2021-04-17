83bca0b94f4e88a353e3ae836c9c8a53e6d3bf3b
supplise <81015704+supplise@users.noreply.github.com>
2021-04-17 22:15:46 +0900

    Update js.js

--- 마도요/js/js.js
+++ 마도요/js/js.js
@@ -1,4 +1,4 @@
-const a=["강수철","고구범","곽성훈","김경래","김태헌","김태현","영훈","안재혁","연우","예진","이원재","재원","전빈","전준식","정민","정준영","지훈","엄태성","혁종"];
+const 
 const BTN=document.querySelector(".btn");
 const TEXT_BOX=document.querySelector(".text_box span");
 
